ed6a795bcd05e81f3a1322c55fc80ce482550f81
William Garrett <will.garrett2k@gmail.com>
2016-06-13 11:13:34 -0700

    Initial

--- configuration.php
+++ configuration.php
@@ -3,7 +3,7 @@
 * 
 ************************************************/
 
-    define("API_URL", https://pcc.populiweb.com/api/index.php"); // * required 
+    define("API_URL", "ENTER DOMAIN/api/index.php"); // * required 
     
-    define("API_TOKEN", "764319210f3847889173c896de3d2d7263fb4d5c2265ba4b93892c7a7d45ad657136d7c60f4c2d5c225a2bd84283f27f1decffa612b9e8ff68ff6e11adc6240c6520ab7a96da60b4aec1e4d64769f58df6969a6a0b74f0f9eeddb9c484c233632e5c224e617b17cfb6724960dccbd81ddf7496c064c60b4e6ee94ffb78cc530caf5d3971965c5cb394e351a239d91c901c6754b65c306b0fcbe157dfb0cb3ba65c2298bedb5e987901547e818e98137b"); // Either token required or login credentials required at populi class instantiation
+    define("API_TOKEN", "ENTER API TOKEN"); // Either token required or login credentials required at populi class instantiation
 ?>
